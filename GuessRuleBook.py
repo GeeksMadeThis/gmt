dc729b85f3168b023ecd36375a0126bf56a8112d
John Donnellan <john@blueTribu.com>
2017-08-17 20:23:01 +0100

    add GetNewBoard to Guess

--- GuessRuleBook.py
+++ GuessRuleBook.py
@@ -1,4 +1,27 @@
+import random
+
+class GuessBoard:
+    def __init__(self, min, max, target):
+        self.min = min
+        self.max = max
+        self.target = target
 
 
+class GuessRuleBook:
 
-print("Hello World!")
+    def GetNewBoard(boardOptions):
+        min = (boardOptions and boardOptions[min]) or 1
+        max = (boardOptions and boardOptions[max]) or 6
+        if not isinstance(min, int): raise ValueError(f"min must be a whole number: {min}")
+        if not isinstance(max, int): raise ValueError(f"max must be a whole number: {max}")
+        if min > max: raise ValueError(f"min () must not be greater than max ()")
+        target = random.randint(min, max)
+        return GuessBoard(min, max, target)
+
+
+print("Let's Play!")
+
+ruleBook = GuessRuleBook
+board = ruleBook.GetNewBoard(None)
+
+print(board.target)
