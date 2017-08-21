6328de01c3953a70a4e6b4c387b3295d3fc7ffc1
G.L.O.O.M <TheVainBoy@users.noreply.github.com>
2017-08-21 19:41:58 +0100

    Complete Task

--- GuessRuleBook.py
+++ GuessRuleBook.py
@@ -12,10 +12,11 @@ class GuessRuleBook:
     def GetNewBoard():
         min = 1
         max = 6
-        if not isinstance(min, int): raise ValueError(f"min must be a whole number: {min}")
-        if not isinstance(max, int): raise ValueError(f"max must be a whole number: {max}")
-        if min > max: raise ValueError(f"min:{min} must not be greater than max:{max}")
-        target = # put something here.
+
+        if not isinstance(min, int): raise ValueError(f"The minimum value must be a whole number: {min}")
+        if not isinstance(max, int): raise ValueError(f"The maximum value must be a whole number: {max}")
+        if min > max: raise ValueError(f"The minimum value:{min} must not be greater than maximum value:{max}")
+        target = random.randint(min, max)
         return GuessBoard(min, max, target)
 
 
